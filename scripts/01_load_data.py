bb44d6a5e1699bd575ec5da554330bfbe84b06a4
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-24 20:57:10 -0800

    01_load_data script outline

---
+++ scripts/01_load_data.py
@@ -0,0 +1,8 @@
+# 01_load_data.py
+# 1. Import necessary libraries (pandas, os)
+# 2. Define function `load_data(file_path)`:
+#    a. Read the dataset using pandas
+#    b. Drop missing values if necessary
+# 3. If run as the main script:
+#    a. Load dataset from 'data/' folder
+#    b. Save the cleaned dataset to 'outputs/' folder
